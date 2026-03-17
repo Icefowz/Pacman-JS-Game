df2addb7a649b921de765b2b398f94ba6237a319
Icefowz <maimoy16@gmail.com>
2026-03-17 15:10:35 +0200

    Generate Map Boundaries

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Boundary {
+  static width = 40;
+  static height = 40;
   constructor({ position }) {
     this.position = position;
     this.width = 40;
@@ -16,12 +18,30 @@ class Boundary {
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
-
-const boundary = new Boundary({
-  position: {
-    x: 0,
-    y: 0,
-  },
+const map = [
+  ["-", "-", "-", "-", "-", "-"],
+  ["-", "", "", "", "", "-"],
+  ["-", "", "-", "-", "", "-"],
+  ["-", "", "", "", "", "-"],
+  ["-", "-", "-", "-", "-", "-"],
+];
+const boundaries = [];
+map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    switch (symbol) {
+      case "-":
+        boundaries.push(
+          new Boundary({
+            position: {
+              x: Boundary.width * j,
+              y: Boundary.height * i,
+            },
+          }),
+        );
+        break;
+    }
+  });
 });
-
-boundary.draw();
+boundaries.forEach((boundary) => {
+  boundary.draw();
+});
