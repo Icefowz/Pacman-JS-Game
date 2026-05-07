73f41101918558feff8882547e9d61114312ede1
Dokusen <ChrisTsikrikes@gmail.com>
2026-05-07 16:11:36 +0300

    Add ghost collision (#4)
    * Update index.js
    
    * Update index.js
    
    * Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class Boundary {
     this.image = image;
   }
 
-  draw() {
+  draw() {  
     // c.fillStyle = "blue";
     // c.fillRect(this.position.x, this.position.y, this.width, this.height);
     c.drawImage(this.image, this.position.x, this.position.y);
@@ -43,6 +43,33 @@ class Player {
     this.position.y += this.velocity.y;
   }
 }
+
+class Ghost {
+  static speed = 1;
+  constructor({ position, velocity, color = 'red' }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 18;
+    this.color = color;
+    this.prevCollisions = [];
+    this.speed = 1;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.closePath();
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 class Pellet {
   constructor({ position }) {
     this.position = position;
@@ -59,6 +86,29 @@ class Pellet {
 }
 const pellets = [];
 const boundaries = [];
+const ghosts = [
+  new Ghost({
+    position: {
+      x: Boundary.width * 6 + Boundary.width / 2,
+      y: Boundary.height * 7 + Boundary.height / 2,
+    },
+    velocity: {
+      x: 1,
+      y: 0
+    }
+  }),
+   new Ghost({
+    position: {
+      x: Boundary.width * 6 + Boundary.width / 2,
+      y: Boundary.height * 3 + Boundary.height / 2,
+    },
+    velocity: {
+      x: 1,
+      y: 0
+    },
+    color :'blue'
+  }),
+];
 const player = new Player({
   position: {
     x: Boundary.width + Boundary.width / 2,
@@ -172,7 +222,7 @@ map.forEach((row, i) => {
           new Boundary({
             position: {
               x: Boundary.width * j,
-              y: Boundary.height * i,
+              y: Boundary.height * i, 
             },
             image: createImage("./img/pipeCorner4.png"),
           }),
@@ -306,20 +356,22 @@ map.forEach((row, i) => {
 });
 
 function circleCollidesWithRectangle({ circle, rectangle }) {
+  const padding = Boundary.width / 2 - circle.radius - 1;
   return (
     circle.position.y - circle.radius + circle.velocity.y <=
-      rectangle.position.y + rectangle.height &&
+      rectangle.position.y + rectangle.height + padding &&
     circle.position.x + circle.radius + circle.velocity.x >=
-      rectangle.position.x &&
+      rectangle.position.x - padding &&
     circle.position.y + circle.radius + circle.velocity.y >=
-      rectangle.position.y &&
+      rectangle.position.y - padding &&
     circle.position.x - circle.radius + circle.velocity.x <=
-      rectangle.position.x + rectangle.width
+      rectangle.position.x + rectangle.width + padding
   );
 }
 
+let animationId;
 function animate() {
-  requestAnimationFrame(animate);
+  animationId = requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   if (keys.w.pressed && lastKey === "w") {
@@ -414,8 +466,111 @@ function animate() {
     }
   });
   player.update();
-  // player.velocity.x = 0;
-  // player.velocity.y = 0;
+  
+  ghosts.forEach((ghost) => {
+    ghost.update();
+
+      if (
+      Math.hypot(
+        ghost.position.x - player.position.x,
+        ghost.position.y - player.position.y,
+      ) <
+      ghost.radius + player.radius
+    ) {
+      cancelAnimationFrame(animationId);
+      alert("Game Over");
+      console.log("Game Over");
+    }
+
+    const collisions = [];
+    boundaries.forEach((boundary) => {
+      if (
+        !collisions.includes('right') && 
+        circleCollidesWithRectangle({
+          circle: { ...ghost, velocity: { x: 1, y: 0 } },
+          rectangle: boundary,
+        })
+      ) {
+        collisions.push('right');
+      }
+
+      if (
+        !collisions.includes('left') && 
+        circleCollidesWithRectangle({
+          circle: { ...ghost, velocity: { x: -1, y: 0 } },
+          rectangle: boundary,
+        })
+      ) {
+        collisions.push('left');
+      }
+
+      if (
+        !collisions.includes('down') && 
+        circleCollidesWithRectangle({
+          circle: { ...ghost, velocity: { x: 0, y: 1 } },
+          rectangle: boundary,
+        })
+      ) {
+        collisions.push('down');
+      }
+
+      if (
+        !collisions.includes('up') && 
+        circleCollidesWithRectangle({
+          circle: { ...ghost, velocity: { x: 0, y: -1 } },
+          rectangle: boundary,
+        })
+      ) {
+        collisions.push('up');
+      }
+    });
+
+    if (collisions.length > ghost.prevCollisions.length) 
+    ghost.prevCollisions = collisions;
+  
+    if (JSON.stringify(collisions) !== JSON.stringify(ghost.prevCollisions)) {
+      
+      if(ghost.velocity.x > 0) ghost.prevCollisions.push('right');
+      else if(ghost.velocity.x < 0) ghost.prevCollisions.push('left');
+      else if(ghost.velocity.y > 0) ghost.prevCollisions.push('down');
+      else if(ghost.velocity.y < 0) ghost.prevCollisions.push('up');
+
+      const pathways = ghost.prevCollisions.filter((collision) => {
+          return !collisions.includes(collision);
+      
+      });
+      console.log({ pathways });
+
+      const direction = pathways[Math.floor(Math.random() * pathways.length)];
+      console.log({ direction });
+
+      switch (direction) {
+        case 'down': 
+          ghost.velocity.y = Ghost.speed;
+          ghost.velocity.x = 0;
+          break;
+    }
+      switch (direction) {
+        case 'up': 
+          ghost.velocity.y = -Ghost.speed;
+          ghost.velocity.x = 0;
+          break;
+    }
+      switch (direction) {
+        case 'right': 
+          ghost.velocity.y = 0;
+          ghost.velocity.x = Ghost.speed;
+          break;
+    }
+      switch (direction) {
+        case 'left': 
+          ghost.velocity.y = 0;
+          ghost.velocity.x = -Ghost.speed;
+          break;
+    }
+        ghost.prevCollisions = [];
+    }
+  });
 }
 animate();
 
